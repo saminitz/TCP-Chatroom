84d36bbe5529947147a538aae1fb5135d9adf351
Samuel Nitzsche <samuel.nitzsche@volkswagen.de>
2023-01-09 13:13:10 +0100

    - Added /rename with message to other users - Refactored variable names

--- broadcast.js
+++ broadcast.js
@@ -1,13 +1,13 @@
 const global = require('./global');
 const user = require('./user');
 
-function echoToAllSockets(socket, data) {
+function echoToAllSockets(socket, message) {
   let username = user.getUsername(socket);
   for (let i = 0; i < global.allSockets.length; i++) {
     if (global.allSockets[i].socket != socket) {
       global.allSockets[i].socket.write(
         '\r' + user.usernameAndSpacing(username)
-        + data + '\r\n'
+        + message + '\r\n'
         + user.usernameAndSpacing(global.allSockets[i].username));
     }
   }
