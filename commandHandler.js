53ab62590f464f23a774ad8c835be89be3bfc1d2
Samuel Nitzsche <49833818+saminitz@users.noreply.github.com>
2023-01-10 16:00:12 +0100

    work in progress command handler

--- commandHandler.js
+++ commandHandler.js
@@ -4,38 +4,61 @@ const user = require('./user');
 const server = require('./server');
 const messaging = require('./messaging');
 
+const commands = {
+  "rename": {
+    "run": (socket, username) => {
+      if (username.length > 0) {
+        let oldUsername = user.getUsername(socket);
+        user.setUsername(global.getConnection(socket), username, true);
+        messaging.broadcastRaw(socket, oldUsername + ' hat sich zu ' + user.getUsername(socket) + ' umbenannt');
+      }
+    },
+    "usage": "/rename name",
+    "man": "Mit rename kann der Benutzername geändert werden, dabei werden alle Leerzeichen und Umbrüche entfernt."
+  }
+}
+
 function query(socket, message) {
   if (!message.startsWith('/')) {
     return;
   }
 
-  let command = message.split(' ');
-  switch (command[0].toLowerCase()) {
-    case '/rename':
-      if (message.length > '/rename'.length + 1) {
-        let oldUsername = user.getUsername(socket);
-        user.setUsername(global.getConnection(socket), message.substr(command[0].length + 1), true);
-        messaging.broadcastRaw(socket, oldUsername + ' hat sich zu ' + user.getUsername(socket) + ' umbenannt');
-      }
-      break;
+  let command = message.substr(1).split(' ')[0];
+  let parameter = message.substr(command.length + 1);
 
-    case '/users':
-    case '/list':
-      usernames = []
-      for (const socketItem of global.allConnections) {
-        usernames.push(socketItem.username);
-      }
-      messaging.sendRawLineAndUser(socket, "Currently connected users are: " + usernames.join(", ") + "\r\n");
-      break;
-
-    case '/logout':
-      server.closeSocketConnection(socket);
-      break;
-
-    default:
-      messaging.sendRawLineAndUser(socket, "Command invalid");
-      break;
+  if (commands[command] == undefined) {
+    messaging.sendRawLineAndUser(socket, "Command invalid");
+  } else {
+    commands[command].run(socket, parameter);
   }
+
+  // switch (command[0].toLowerCase()) {
+  //   case '/rename':
+
+  //     break;
+
+  //   case '/users':
+  //   case '/list':
+  //     usernames = []
+  //     for (const socketItem of global.allConnections) {
+  //       usernames.push(socketItem.username);
+  //     }
+  //     messaging.sendRawLineAndUser(socket, "Currently connected users are: " + usernames.join(", ") + "\r\n");
+  //     break;
+
+  //   case '/logout':
+  //     server.closeSocketConnection(socket);
+  //     break;
+
+  //   case '/help':
+  //     messaging.sendRawLine(
+  //       ''
+  //     )
+  //     break;
+
+  //   default:
+  //     break;
+  // }
 
   throw new customError.StopParent('command')
 }
